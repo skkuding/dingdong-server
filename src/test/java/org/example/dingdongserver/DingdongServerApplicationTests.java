27305115b32146b24a0bb954fe2490eeb6e5187d
w0uldy0u <spacekim01@gmail.com>
2026-03-16 21:23:57 +0900

    chore: use mysql rather than mariadb

--- src/test/java/org/example/dingdongserver/DingdongServerApplicationTests.java
+++ src/test/java/org/example/dingdongserver/DingdongServerApplicationTests.java
@@ -2,9 +2,21 @@ package org.example.dingdongserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.docker.compose.enabled=false")
+@Testcontainers
 class DingdongServerApplicationTests {
+
+    @Container
+    @ServiceConnection
+    static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.4")
+            .withDatabaseName("dingdong")
+            .withUsername("dingdong")
+            .withPassword("1234");
 
     @Test
     void contextLoads() {
